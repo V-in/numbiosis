125b16d7cef8e7bb15f0b28d4b4f6f684eccd74e
V-in <vinicius_gbapb@hotmail.com>
2018-10-02 20:14:08 -0300

    Added more columns to mathjax example matrix

--- pages/mathjax.js
+++ pages/mathjax.js
@@ -2,11 +2,11 @@ import Mathjax from 'react-mathjax'
 import MethodsLayout from '../layouts/MethodsLayout'
 
 //Exemplo de entrada
-const initial = [[1, 2, 3], [3, 4, 5], [1, 2, 3]]
+const initial = [[1, 2, 3, 5], [3, 4, 5, 1], [1, 2, 3, 5]]
 
 //TODO: Funçao que retorna latex de uma matriz a partir de um objeto do tipo [[float]] - array de arrays
 const mapDataToArgs = (data = initial) => {
-    `
+    return `
         a=3
     `
 }
